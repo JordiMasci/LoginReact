d11511de238215e81f1376593d41c4ea758be538
Jordi <jordimasci96@gmail.com>
2025-02-19 17:52:48 +0100

    Modale e button acceso/spento

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,6 +14,8 @@ function Login() {
   const navigate = useNavigate();
 
   const [error, setError] = useState(false);
+
+  const [focusButton, setFocusButton] = useState(false);
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -46,12 +48,24 @@ function Login() {
     const { value, name, type, checked } = e.target;
     const inputValue = type === "checkbox" ? checked : value;
 
-    setForm((form) => ({
-      ...form,
-      [name]: inputValue,
-    }));
+    setForm((prevForm) => {
+      const updatedForm = { ...prevForm, [name]: inputValue };
+
+      validateForm(updatedForm);
+
+      return updatedForm;
+    });
   };
 
+  const validateForm = (updatedForm) => {
+    const emailValid =
+      updatedForm.email.includes("@") && updatedForm.email.includes(".");
+    const passwordValid = updatedForm.password.length >= 8;
+
+    setFocusButton(emailValid && passwordValid);
+  };
+
+  // SUBMIT VALIDATION
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -75,8 +89,6 @@ function Login() {
     });
   };
 
-  // console.log(users);
-
   return (
     <>
       <section className="LoginContainer">
@@ -88,11 +100,63 @@ function Login() {
             className="w-full flex flex-col justify-center max-w-sm"
             onSubmit={handleSubmit}
           >
+            {/* MODALE */}
             {error && (
-              <div className="flex justify-center italic bold">
-                <p className="text-red-500 text-md mb-4">
-                  Email o password non corretti
-                </p>
+              <div
+                className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-gray-900 bg-opacity-50"
+                onClick={() => setError(false)}
+              >
+                <div className="relative p-4 w-full max-w-md bg-white rounded-lg shadow-sm dark:bg-gray-700">
+                  <button
+                    type="button"
+                    className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={() => setError(false)}
+                  >
+                    <svg
+                      className="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                  <div className="p-4 md:p-5 text-center">
+                    <svg
+                      className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                      Password o Email non valide
+                    </h3>
+                    <button
+                      onClick={() => setError(false)}
+                      type="button"
+                      className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                    >
+                      RIPROVA
+                    </button>
+                  </div>
+                </div>
               </div>
             )}
 
@@ -105,6 +169,7 @@ function Login() {
               </label>
               <input
                 onChange={handleChange}
+                onBlur={() => validateForm(form)}
                 value={form.email}
                 name="email"
                 type="email"
@@ -123,11 +188,15 @@ function Login() {
               </label>
               <input
                 onChange={handleChange}
+                onBlur={() => validateForm(form)}
                 value={form.password}
                 name="password"
                 type="password"
                 id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
+                  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                   dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
                 placeholder="Enter password"
               />
@@ -140,7 +209,9 @@ function Login() {
                   id="remember"
                   type="checkbox"
                   checked={form.isChecked}
-                  className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3
+                   focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600
+                    dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                 />
               </div>
               <label
@@ -152,7 +223,12 @@ function Login() {
             </div>
             <button
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className={`${
+                focusButton
+                  ? "bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                  : "bg-gray-200  dark:bg-gray-600  "
+              } text-white font-medium rounded-lg text-sm w-full sm:w-auto 
+              px-5 py-2.5 text-center `}
             >
               Accedi
             </button>
