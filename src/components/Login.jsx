4730e369d96f9709466fd6760010abe67614510e
Jordi <jordimasci96@gmail.com>
2025-02-19 09:22:20 +0100

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -134,7 +134,7 @@ function Login() {
                 name="isChecked"
                 id="remember"
                 type="checkbox"
-                checked={form.check}
+                checked={form.isChecked}
                 className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
               />
             </div>
