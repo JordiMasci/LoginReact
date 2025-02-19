b2b304498e9563f15560a851387e4a2be5fb3b08
Jordi <jordimasci96@gmail.com>
2025-02-19 16:34:54 +0100

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -110,7 +110,7 @@ function Login() {
                 type="email"
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Inserisci email"
+                placeholder="Enter email"
                 required
               />
             </div>
@@ -129,7 +129,7 @@ function Login() {
                 id="password"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
-                placeholder="Inserisci Password"
+                placeholder="Enter password"
               />
             </div>
             <div className="flex items-start mb-5">
