af323359fc06407b2a2fdea7418e3b5f0284960a
Jordi <jordimasci96@gmail.com>
2025-02-26 14:52:55 +0100

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -113,7 +113,7 @@ const Navbar = () => {
             {profile.superAdmin ? (
               <li>
                 <Link
-                  to={"/ceateUser"}
+                  to={"/createUser"}
                   className="block py-2 px-3 text-gray-900 rounded
                  hover:bg-gray-500 md:hover:bg-transparent
                  md:hover:text-blue-700 md:p-0 dark:text-white"
--- src/main.jsx
+++ src/main.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
     element: <EditUser></EditUser>,
   },
   {
-    path: "/ceateUser",
+    path: "/createUser",
     element: <CreateUser></CreateUser>,
   },
 ]);
